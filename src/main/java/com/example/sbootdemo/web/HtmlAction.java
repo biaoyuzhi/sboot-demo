3e2c5d19cb770156767fb42d8b133618e34d98c5
biaoyuzhi <bi405yu0115zZ>
2019-11-23 13:38:26 +0800

    添加简单的前端表单提交

--- src/main/java/com/example/sbootdemo/web/HtmlAction.java
+++ src/main/java/com/example/sbootdemo/web/HtmlAction.java
@@ -1,9 +1,12 @@
 package com.example.sbootdemo.web;
 
 import com.example.sbootdemo.interceptor.NeedLogin;
+import com.example.sbootdemo.pojo.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 /**
  * Created by wuzh on 2019/9/12.
@@ -23,6 +26,13 @@ public class HtmlAction {
     public String loginIndex(Model model){
         int[] data = {10,23,65,16,97,32};
         model.addAttribute("data",data);
+        return "index";
+    }
+
+    @RequestMapping(value = "/login", method = RequestMethod.POST)
+    public String login(User user){     // 这里可以直接使用User对象来接收前端传过来的表单数据，根据表单的name属性名称一一对应
+        System.out.println(user.toString());
+        // 如果不引入模板依赖，templates下的html文件是访问不到的，会抛出405等异常
         return "index";
     }
 }
